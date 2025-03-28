8d9a87febe8e14ff997b9119ffa6fd2ad0242cab
Ben Juntilla <ben@benjuntilla.com>
2025-03-28 03:34:19 -0700

    just add it ourselves

--- ben-guix/packages/editors.scm
+++ ben-guix/packages/editors.scm
@@ -1,41 +1,93 @@
 (define-module (ben-guix packages editors)
-  #:use-module (guix packages)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages gtk)
   #:use-module (guix download)
-  #:use-module (guix git-download)
-  #:use-module (guix utils)
-  #:use-module (guix upstream)
-  #:use-module (guix build utils)
-  #:use-module (srfi srfi-1)
-  #:use-module (json)
-  #:use-module (web client)
-  #:use-module (web response)
-  #:use-module (ice-9 match)
-  #:use-module (nongnu packages editors))
+  #:use-module (guix gexp)
+  #:use-module (guix packages)
+  #:use-module ((guix licenses) :prefix license:)
+  #:use-module (nonguix build-system chromium-binary)
+  #:use-module (ice-9 match))
 
-;; Define the updater
-(define %vscodium-updater
-  (upstream-updater
-   (name 'vscodium)
-   (description "Updater for VSCodium")
-   (pred (lambda (package)
-           (string=? (package-name package) "vscodium")))
-   (latest
-    (lambda* (package #:key (version #f))
-      (let* ((url "https://api.github.com/repos/VSCodium/vscodium/releases/latest")
-             (response (http-get url #:headers '((user-agent . "Guix"))))
-             (json (json-string->scm (response-body-port response))))
-        (let ((tag (assoc-ref json "tag_name")))
-          (upstream-source
-           (package (package-name package))
-           (version tag)
-           (urls (map (lambda (arch)
-                        (string-append
-                         "https://github.com/VSCodium/vscodium/releases/download/" tag
-                         "/VSCodium-linux-" arch "-" tag ".tar.gz"))
-                      '("x64" "arm64" "armhf")))))))))
-
-;; Register the updater
-(register-updater %vscodium-updater)
-
-;; Re-export the original vscodium package
-(define-public vscodium (@@ (nongnu packages editors) vscodium))
+(define-public ben-vscodium
+  (package
+    (name "ben-vscodium")
+    (version "1.98.2.25078")
+    (source
+     (let ((arch (match (or (%current-target-system) (%current-system))
+                   ("aarch64-linux" "arm64")
+                   ("armhf-linux" "armhf")
+                   (_ "x64")))
+           (hash (match (or (%current-target-system) (%current-system))
+                   ("aarch64-linux"
+                    "lol")
+                   ("armhf-linux"
+                    "lol")
+                   (_
+                    "02mrksh3fn4k04aprcadg40cib2j9sm8vsamqpldfglwyjn8495a"))))
+       (origin
+        (method url-fetch)
+        (uri
+         (string-append
+          "https://github.com/VSCodium/vscodium/releases/download/" version
+          "/VSCodium-linux-" arch "-" version ".tar.gz"))
+        (sha256
+         (base32 hash)))))
+    (build-system chromium-binary-build-system)
+    (arguments
+     (list #:validate-runpath? #f ; TODO: fails on wrapped binary and included other files
+           #:substitutable? #f
+           #:wrapper-plan
+           #~'(("opt/vscodium/codium" (("out" "/opt/vscodium"))))
+           #:phases
+           #~(modify-phases %standard-phases
+               (replace 'unpack
+                 (lambda* (#:key source #:allow-other-keys)
+                   (mkdir-p "opt/vscodium")
+                   (invoke "tar" "-xvf" source "-C" "opt/vscodium")))
+               (add-before 'install-wrapper 'install-entrypoint
+                 (lambda _
+                   (let* ((bin (string-append #$output "/bin")))
+                     (delete-file (string-append #$output "/environment-variables"))
+                     (mkdir-p bin)
+                     (symlink (string-append #$output "/opt/vscodium/codium")
+                              (string-append bin "/codium")))))
+               (add-after 'install-entrypoint 'install-resources
+                 (lambda _
+                   (let* ((icons
+                           (string-append #$output
+                                          "/share/icons/hicolor/512x512/apps"))
+                          (icon.png
+                           (string-append #$output
+                                          "/opt/vscodium/resources/app/"
+                                          "resources/linux/code.png"))
+                          (apps (string-append #$output "/share/applications")))
+                     (mkdir-p icons)
+                     (symlink icon.png
+                              (string-append icons "/code.png"))
+                     (mkdir-p apps)
+                     (make-desktop-entry-file
+                      (string-append apps "/" #$name ".desktop")
+                      #:name "VSCodium"
+                      #:generic-name "Text Editor"
+                      #:exec (string-append #$output "/bin/codium --ozone-platform-hint=auto")
+                      #:icon "code"
+                      #:type "Application"
+                      #:actions '("new-empty-window")
+                      #:keywords '("vscode")
+                      #:categories '("TextEditor" "Development"
+                                     "IDE")
+                      #:startup-notify #t
+                      #:startup-w-m-class "Code"
+                      #:comment
+                      '(("en" "Code Editing. Redefined.")
+                        (#f "Code Editing. Redefined.")))))))))
+    (supported-systems '("armhf-linux" "aarch64-linux" "x86_64-linux"))
+    (native-inputs
+     (list tar))
+    (inputs
+     (list gdk-pixbuf))
+    (home-page "https://vscodium.com/")
+    (synopsis "Community-driven, freely-licensed binary distribution of VSCode")
+    (description "VSCodium is a community-driven, freely-licensed binary
+distribution of Microsoft's editor VSCode.")
+    (license license:expat)))
